e97ec04ff9d7c301f85fc7baa0bf7e6021db5e51
bayoishola20 <bayoishola20@yahoo.com>
2018-04-24 15:14:10 +0100

    Geo

--- Geo/geometry.py
+++ Geo/geometry.py
@@ -1,6 +1,15 @@
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, Point, mapping, shape
+import json
 
 poly = Polygon([(0,0), (0,5), (5,5), (5,0)])
 print poly
 print('area', poly.area)
 print('geometry', poly.geom_type)
+
+patch = Point(12.0, 5.0).buffer(15.0)
+print patch.area
+
+# Shapely does not read and write data files but serializes and deserializes
+a = shape(json.loads('{"type": "Point", "coordinates": [6.2, 3.3] }'))
+print a
+print json.dumps(mapping(a))
