9949160bc15d2b8f45d108991ff686df90e8db82
bayoishola20 <bayoishola20@yahoo.com>
2018-05-28 04:16:25 +0100

    web scapping intro

---
+++ Random/web_scrapping.py
@@ -0,0 +1,14 @@
+# python3 web_scrapping.py
+import requests
+import bs4 #beautifulsoup
+
+req = requests.get('https://learncodeonline.in/')
+type(req)
+# req.text
+
+soup = bs4.BeautifulSoup(req.text, 'lxml')
+type(soup)
+
+links = soup.select('img')
+for link in links:
+    print(link, '\n')
