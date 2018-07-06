b4a3fa6e9c5824b07bbc508b998e3e3bbec3585f
bayoishola20 <bayoishola20@yahoo.com>
2018-07-06 02:46:15 +0100

    self kNearest

--- ML/self_kNearest.py
+++ ML/self_kNearest.py
@@ -23,5 +23,21 @@ plt.show() """
 def kNearest(data, predict, k=3):
     if len(data) >= k:
         warnings.warn('k should be higher than the total voting groups!')
-    knnalgos
+    distances = []
+    for group in data:
+        for features in data[group]:
+            # euclidean_dist = sqrt( (features[0] - predict[0])**2 + (features[1] - predict[1])**2 )
+            # euclidean_dist = np.sqrt( np.sum( (np.array(features) - np.array(predict)) **2) )
+            euclidean_dist = np.linalg.norm(np.array(features) - np.array(predict))
+            distances.append([euclidean_dist, group])
+    votes = [ i[1] for i in sorted(distances)[:k] ]
+    print Counter(votes).most_common(1)
+    vote_result = Counter(votes).most_common(1)[0][0]
     return vote_result
+
+print "kNearest is:", kNearest(dataset, new_features, k=3)
+
+result = kNearest(dataset, new_features, k=3)
+[[plt.scatter(j[0], j[1], s=100, c=i) for j in dataset[i]] for i in dataset]
+plt.scatter(new_features[0], new_features[1], s=50, c=result)
+plt.show()
