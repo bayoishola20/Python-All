a08deb8a3b4db2c6b36a3b1a7375875dd226aedd
bayoishola20 <bayoishola20@yahoo.com>
2018-09-19 23:41:43 +0100

    SVM & kernel

--- Random/quadratic_prob.py
+++ Random/quadratic_prob.py
@@ -1,6 +1,6 @@
 ''' CVXOPT - Python Software for Convex Optimization.
 https://cvxopt.org/examples/tutorial/qp.html
-Using python 3 here.
+
 '''
 from cvxopt import matrix, solvers
 Q = 2*matrix([ [2, .5], [.5, 1] ])
@@ -11,4 +11,4 @@ A = matrix([1.0, 1.0], (1,2))
 b = matrix(1.0)
 sol=solvers.qp(Q, p, G, h, A, b)
 
-print(sol['x'])
+print sol['x']
