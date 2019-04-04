d01925d57973d544f4bf9e47c85a2f67d6f02410
bayoishola20 <bayoishola20@yahoo.com>
2019-04-04 05:55:59 +0200

    TwitterAPI

--- Geo/Twitter/TwitterAPI/Visualization/assets/script.js
+++ Geo/Twitter/TwitterAPI/Visualization/assets/script.js
@@ -29,7 +29,14 @@ let test = noUiSlider.create(slider, {
         'min': 0,
         'max': 100
     },
-})
+});
+
+function toFormat(v) {
+    var tt = new Date(v);
+    return "Date:" + tt.getDate() + "." + (tt.getMonth() + 1) + "."
+    + tt.getFullYear() + " Time:" + tt.getHours() + ":" + tt.getMinutes() + ":"
+    + tt.getSeconds();
+}
 
 let proj = d3.geoEqualEarth();
 // .translate([w / 2, h / 2]) <= can be avoided using .fitExtent()
@@ -92,5 +99,21 @@ Promise.all([
                         .style("opacity", "0.5")
         
         let dateValues = [d3.select("#startEvent").node(),d3.select("#endEvent").node()];
-        console.log(dateValues);
+        //console.log(dateValues);
+
+        slider.noUiSlider.on("update", function(values, handle, c, d, e){
+        let time = [Math.round(+values[0]), Math.round(+values[1])];
+
+        let test = circles //svg.selectAll("circle")
+        .style("fill", "white")
+        .style("opacity", 0) //not visible
+        .filter(function(d) {
+        return d.properties.utcms <= time[1] && d.properties.utcms >= time[0];
+        })
+        .style("fill", "green")
+        .style("opacity", 0.5);
+
+        dateValues[handle].innerHTML = "Date: " + getCurrentDate(new
+            Date(time[handle])) + " Time: " + getCurrentTime(new Date(time[handle]));
+            });
     });
