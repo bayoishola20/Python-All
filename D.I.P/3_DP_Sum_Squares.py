1b1e5781ab011ac8672f99cd2511fbe4b0fbd31d
bayoishola20 <bayoishola20@yahoo.com>
2020-01-03 00:38:56 +0100

    DP

--- D.I.P/3_DP_Sum_Squares.py
+++ D.I.P/3_DP_Sum_Squares.py
@@ -1,23 +1,27 @@
 # Given a number n, find the least number of squares needed to sum up to the number.
 
 def square_sum(n):
-    # minimum squares for 3, 2 and 1 is same as the number itself. Check by solving
+    # minimum squares for 3, 2 and 1 is same as the number itself. Check by solving.
     if n <= 3:
         return n
-    ans = n
+    # check if number is a perfect square return 1
+    elif n == int( pow(n,1/2) ):
+        return 1
+    
+    ans = n # store maximum squares needed
 
     for i in range(1, n+1):
         tmp = i * i
-        print tmp
         if tmp > n:
             break
-        # elif tmp == n:
-        #     ans = 1
+        elif tmp == n:
+            ans = 1
         else:
-            ans = min( ans, 1 + square_sum(n - tmp) )
-
+            ans = min( ans, 1 + square_sum(n - tmp) ) # recursion
     return ans
 
-print square_sum(13)
+print square_sum(100)
 # Min sum is 3^2 + 2^2
 # 2
+
+#PS: This solution works for small values of n.
