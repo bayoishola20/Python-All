092cdcd02b0b2d302244a84c85e1074ddf4acf3a
bayoishola20 <bayoishola20@yahoo.com>
2020-01-03 23:39:51 +0100

    geo

---
+++ Geo/arcgis_arcpy/2_System.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.append("c:\\Users\\Adebayo\\Documents\\Python-All") # append new file path
+print sys.path #list of file paths, which the user can modify to adjust where Python will look for a module to import, without needing administrative access
