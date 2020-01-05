594f6c4d5af5bd1235ac31de832b5ef9b680391f
bayoishola20 <bayoishola20@yahoo.com>
2020-01-05 23:43:10 +0100

    DP

---
+++ D.I.P/4_DP_Anagrams_String.py
@@ -0,0 +1,10 @@
+# Given 2 strings s and t, find and return all indexes in string s where t is an anagram.
+
+def find_anagrams(s, t):
+    pass
+
+
+print find_anagrams('acdbacdacb', 'abc')
+# [3, 7]
+print find_anagrams('AAABABAAB', 'AABA')
+# [0, 1, 4]
