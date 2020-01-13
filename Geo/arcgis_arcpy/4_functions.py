367d58f5329cb6118ba8d6b916f2a880a8a044c4
bayoishola20 <bayoishola20@yahoo.com>
2020-01-14 00:47:12 +0100

    Geo

--- Geo/arcgis_arcpy/4_functions.py
+++ Geo/arcgis_arcpy/4_functions.py
@@ -4,3 +4,26 @@
 def three_multiple(num):
     return "multiple of %i is %i" % (num, num*3)
 print three_multiple(5)
+
+print "\n"
+
+# Funtion above doesn't check for data type of input and could throw an exception
+
+
+# this function validates data type of int and return no value if not int number
+def three_multiple_con(num):
+    if type(num) == type(1) or type(num) == type(1.0):
+        return "CONDITION: multiple of %i is %d" %(num, num*3)
+
+print three_multiple_con(4)
+
+print "\n"
+
+
+# similar function above but with a default parameter and the other takes an input
+
+def three_multiple_param(num, multiplier = 3):
+    if type(num) == type(1) or type(num) == type(1.0):
+        return "PARAMETER: multiple of %i with multiplier %i is %i" %(num, multiplier, num*multiplier)
+
+print three_multiple_param(7, 4)
