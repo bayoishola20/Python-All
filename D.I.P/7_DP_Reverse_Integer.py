8df41ba9ea4abeb17e0604dd7426a578338b6111
bayoishola20 <bayoishola20@yahoo.com>
2020-01-17 23:44:49 +0100

    DP

--- D.I.P/7_DP_Reverse_Integer.py
+++ D.I.P/7_DP_Reverse_Integer.py
@@ -6,7 +6,32 @@ Output: 321 """
 
 class Solution:
     def reverse(self, x):
-        return "will get back to this"
+        answer = 0
+        digits = 0
+        sign = -1
+
+        if x < 0:
+            sign = -1
+        else:
+            sign = 1
+        
+        num = sign * x
+        while num:
+            digits += 1
+            num = num // 10
+        
+        num = sign * x
+        digits -= 1
+        
+        while num:
+            answer = answer + pow(10,digits) * (num%10)
+            num = num // 10
+            digits -= 1
+        
+        if answer > pow(2,31)-1:
+            return 0
+        else:
+            return sign * answer
 
 print Solution().reverse(123)
 # 321
