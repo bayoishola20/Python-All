2a7727c87aefd6ceaa09bb2bdee1619bfad7e468
bayoishola20 <bayoishola20@yahoo.com>
2020-01-23 20:07:03 +0100

    DP

--- D.I.P/8_DP_Primes.py
+++ D.I.P/8_DP_Primes.py
@@ -1,10 +1,24 @@
-'''
+""" 
 Given a positive integer n, find all primes less than n.
+"""
 
-'''
 
 def find_primes(n):
-    pass
+
+    # initialize empty array to store primes
+    result = []
+    for i in range(2, n): # loop through starting from 2 which is the smallest prime number to n
+        isPrime = True # a truthy for prime numbers
+        for j in range(2, i): # loop through starting from 2 to i: So, from loop from 2 to 2, from 2 to 3, then from 2 to 4
+            if i % j == 0: # che
+                isPrime = False
+        if isPrime:
+            result.append(i)
+    return result
+
+    # list comprehension alternative
+    # return [x for x in range(2, n) if all(x % y != 0 for y in range(2, x))]
+
 
 print find_primes(14)
 # [2, 3, 5, 7, 11, 13]
