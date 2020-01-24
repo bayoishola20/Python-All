13a280d45f68cc97c418dc238f9b25a1b9a022c7
bayoishola20 <bayoishola20@yahoo.com>
2020-01-24 13:17:55 +0100

    DP

--- D.I.P/8_DP_Primes.py
+++ D.I.P/8_DP_Primes.py
@@ -10,11 +10,11 @@ def find_primes(n):
     for i in range(2, n): # loop through starting from 2 which is the smallest prime number to n
         isPrime = True # a truthy for prime numbers
         for j in range(2, i): # loop through starting from 2 to i: So, from loop from 2 to 2, from 2 to 3, then from 2 to 4
-            if i % j == 0: # che
-                isPrime = False
-        if isPrime:
-            result.append(i)
-    return result
+            if i % j == 0: # check if each number divided by each has a remainder of zero(0).
+                isPrime = False # then isPrime is False
+        if isPrime: # all other values, that is, that isPrime
+            result.append(i) # append to empty list
+    return result #return result
 
     # list comprehension alternative
     # return [x for x in range(2, n) if all(x % y != 0 for y in range(2, x))]
