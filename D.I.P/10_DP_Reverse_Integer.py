c98f1a57b2d84d83bef961615700e98e0fd1cf21
bayoishola20 <bayoishola20@yahoo.com>
2020-02-02 19:45:44 +0100

    DP

--- D.I.P/10_DP_Reverse_Integer.py
+++ D.I.P/10_DP_Reverse_Integer.py
@@ -1,11 +1,19 @@
 # Given an integer, reverse the digits. Do not convert the integer into a string and reverse it.
 
 def reverse_integer(num):
-    for i in num:
-        print i
+    reverse = 0
+
+    while (num > 0):
+        lastDigit = num % 10 # modulo gets last digit which is the the remainder when you divide by 10
+        reverse = (reverse * 10) + lastDigit # concatenate last digit to reverse: this brings last digit to "Tens" by adding a zero
+        num = num // 10 # remove last digit from number
+    return reverse
   
-print(reverse_integer(135))
+print reverse_integer(135)
 # 531
 
-print(reverse_integer(-321))
+print(reverse_integer(342))
+# 243
+
+print reverse_integer(-321)
 # -123
