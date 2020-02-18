2e2505303f31e1a544aef335ee4e64dff17028e8
bayoishola20 <bayoishola20@yahoo.com>
2020-02-18 21:26:08 +0100

    practice

--- D.I.P/10_DP_Reverse_Integer.py
+++ D.I.P/10_DP_Reverse_Integer.py
@@ -8,6 +8,27 @@ def reverse_integer(num):
         reverse = (reverse * 10) + lastDigit # concatenate last digit to reverse: this brings last digit to "Tens" by adding a zero
         num = num // 10 # remove last digit from number
     return reverse
+    """ answer = 0
+    digits = 0
+    sign = -1 if x < 0 else 1
+    
+    num = sign * x
+    while num:
+        digits += 1
+        num = num // 10
+    
+    num = sign * x
+    digits -= 1
+    
+    while num:
+        answer = answer + pow(10,digits) * (num%10)
+        num = num // 10
+        digits -= 1
+    
+    if answer > pow(2,31)-1:
+        return 0
+    else:
+        return sign * answer """
   
 print reverse_integer(135)
 # 531
