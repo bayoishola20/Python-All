f7c5d5f448c671aad434deadf08d1e307c7e3f1b
bayoishola20 <bayoishola20@yahoo.com>
2020-03-17 16:16:33 +0100

    DP

--- D.I.P/10_DP_Reverse_Integer.py
+++ D.I.P/10_DP_Reverse_Integer.py
@@ -1,34 +1,37 @@
 # Given an integer, reverse the digits. Do not convert the integer into a string and reverse it.
 
 def reverse_integer(num):
-    reverse = 0
+    reverse = 0                                         # fully reversed integer
 
-    while (num > 0):
-        lastDigit = num % 10 # modulo gets last digit which is the the remainder when you divide by 10
-        reverse = (reverse * 10) + lastDigit # concatenate last digit to reverse: this brings last digit to "Tens" by adding a zero
-        num = num // 10 # remove last digit from number
-    return reverse
-    """ answer = 0
-    digits = 0
-    sign = -1 if x < 0 else 1
+    sign = -1 if num < 0 else 1                         # check for integer sign
+
+
+    digits = 0                                          # stores total digits count of integers
     
-    num = sign * x
-    while num:
-        digits += 1
-        num = num // 10
+    x = sign * num                                      # enforces positive integers
+
+    while x:                                            # truth statment for the given integer
+        digits += 1                                     # stores digits counts
+        x = x // 10                                     # gets last digits - floor division that rounds down to nearest whole number
     
-    num = sign * x
-    digits -= 1
+
+    x = sign * num
+
+    digits -= 1                                         # reduces digits count by 1
     
-    while num:
-        answer = answer + pow(10,digits) * (num%10)
-        num = num // 10
+
+    while x:
+        reverse = reverse + pow(10,digits) * (x%10)     # pow(10,digits) gives Hundreds Tens Units of integer and then multiplies by remainder
+
+        x = x // 10                                     # floor division rounded down to nearest whole number
         digits -= 1
     
-    if answer > pow(2,31)-1:
+
+
+    if reverse > pow(2,31)-1:                        # returns False
         return 0
-    else:
-        return sign * answer """
+    else:                                            # else return with sign
+        return sign * reverse
   
 print reverse_integer(135)
 # 531
