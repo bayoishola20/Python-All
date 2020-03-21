6805c37ebc2cf0f38853e32998fb35e79d013290
bayoishola20 <bayoishola20@yahoo.com>
2020-03-21 17:38:19 +0100

    DP

--- D.I.P/12_DP_Majority_Element.py
+++ D.I.P/12_DP_Majority_Element.py
@@ -2,7 +2,10 @@
 # Given a list with a majority element, find the majority element.
 
 def majority_element(nums):
-  # Fill this in.
-  pass
+    unique_elements = set(nums)                                     # get the unique elements
+    freq = nums.count                                               # gets count of each item
+    return max(unique_elements, key=freq)                           # returns the maximum occurence. "Key" is a lambda function that compares integer version of each item of unique elements in nums count.
+
+
 print majority_element([3, 5, 3, 3, 2, 4, 3])
 # 3
