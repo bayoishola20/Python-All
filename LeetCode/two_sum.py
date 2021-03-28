0a9d779281aae6811c0158986dfc8d9b962db0b7
bayoishola20 <bayoishola20@yahoo.com>
2021-03-28 09:53:38 +0200

    two sum

--- LeetCode/two_sum.py
+++ LeetCode/two_sum.py
@@ -18,4 +18,10 @@ class Solution:
 a = Solution()
 
 # Test 1
-print(a.twoSum([3,2,4], 6)) # [1,2]
+print(a.twoSum([3,2,4], 6)) # [1, 2]
+
+# Test 2
+print(a.twoSum([2,7,11,15], 9)) # [0, 1]
+
+# Test 2
+print(a.twoSum([3, 3], 6)) # [0, 1]
